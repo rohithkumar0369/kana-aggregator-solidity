0150b10dcbf31a12771560b77e69a193437381bb
rohithkumar0369 <rohithkumar0369@gmail.com>
2022-11-07 09:32:59 +0530

    initial commit

---
+++ deploy/001_deploy_token.ts
@@ -0,0 +1,35 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+import { ethers } from 'hardhat';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {deployments, getNamedAccounts} = hre;
+  const {deploy} = deployments;
+
+  const {deployer, tokenOwner} = await getNamedAccounts();
+
+  console.log(deployer)
+
+  const diamondCutFacet = await deploy('DiamondCutFacet', {
+    from: deployer,
+    log: true,
+  })
+
+  const diamondLoupeFacet = await deploy('DiamondLoupeFacet', {
+    from: deployer,
+    log: true,
+  })
+
+  const ownershipFacet = await deploy('OwnershipFacet', {
+    from: deployer,
+    log: true,
+    // deterministicDeployment: true,
+  })
+
+  const DimondfacetAddress = await ethers.getContract('DiamondCutFacet')
+  console.log("dimaond address",DimondfacetAddress.address)
+  
+};
+export default func;
+func.id = 'deploy_initial_facets'
+func.tags = ['InitialFacets'];
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,33 +1,38 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import {HardhatUserConfig} from 'hardhat/types';
+import 'hardhat-deploy';
+import 'hardhat-deploy-ethers';
+import {node_url, accounts} from './utils/network';
 
-// Go to https://www.alchemyapi.io, sign up, create
-// a new App in its dashboard, and replace "KEY" with its key
+
 const proxy_url = 'https://devnet.neonevm.org';
 const network_id = 245022926;
 
-const ALCHEMY_API_KEY = "-wPvufVHMDJUtNFRLKXLW0HTDQaPV6ez";
-
-const PRIVATE_KEY = "7fe1d94e8e2c60c51d836a7a6706cb667f2a39e1299602ed9cc0e09a0dad8996";
-
-
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: '0.8.17',
+  },
+  defaultNetwork:'neonlabs',
   networks: {
-    goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [PRIVATE_KEY]
-    },
     neonlabs: {
       url: proxy_url,
-      accounts: [PRIVATE_KEY],
-      // network_id: network_id,
-      chainId: network_id,
-      allowUnlimitedContractSize: false,
-      timeout: 1000000,
-      // isFork: true
-    }
-  }
+      accounts: accounts('neonlabs'),
+         // network_id: network_id,
+         chainId: network_id,
+         allowUnlimitedContractSize: false,
+         timeout: 1000000,
+         // isFork: true
+    },
+    goerli: {
+      url: node_url('goerli'),
+      accounts: accounts('goerli')
+    },
+  },
+  namedAccounts: {
+    deployer: 0,
+    tokenOwner: 1,
+  },
+  paths: {
+    sources: 'src',
+  },
 };
-
 export default config;
